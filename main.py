a417f1f96d3d5bcc5e0d3c7592833ea1161e4dde
Hannah <hannahdlcs@gmail.com>
2023-05-05 10:26:39 -0300

    Created buttons that know where and how they were pressed

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 import tkinter as tk
 from tkinter import ttk
 
+def click(event):
+    y=event.widget.grid_info()['row']
+    x=event.widget.grid_info()['column']
+    print(event.num,x,y)
+
+
+
 #Creates window, defines dimensions and title.
 window = tk.Tk()
-window.config(width=400, height=400)
+window.geometry("400x400")
 window.title("Test")
 
-#Creates button with base (window), text and command
-button = ttk.Button(
-        window,
-        text="")
+#Creates buttons' frames
+frame_button = tk.Frame(window,
+                        width = 20,
+                        height = 20,
+                        bg = "blue",
+                        highlightbackground = "black",
+                        highlightthickness = "1"
+                        )
 
-#Positions button
-button.place(x=300,y=200)
+frame_button.pack_propagate(False)
+frame_button.pack(
+                  expand=True
+                  )
+frame_button.grid(column = 0,
+                  row = 0)
 
-#button.pack(
-#        ipadx=5,
-#        ipady=5,
-#        expand=True)
+frame_button2 = tk.Frame(window,
+                        width = 20,
+                        height = 20,
+                        bg = "blue",
+                        highlightbackground = "black",
+                        highlightthickness = "1"
+                        )
 
+frame_button2.pack_propagate(False)
+frame_button2.grid(column = 1,
+                  row = 0)
+
+#Set buttons' use
+frame_button.bind("<Button>", click)
+frame_button2.bind("<Button>", click)
 
 #Keeps the window open untill closure
 window.mainloop()
